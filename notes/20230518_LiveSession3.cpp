212c0b313dedd1a3faf37099edea00d985d692dc
Adam T Koehler, PhD <akoehler@uic.edu>
2023-05-16 16:52:42 -0500

    initialize notes for May 18, 2023

---
+++ notes/20230518_LiveSession3.cpp
@@ -0,0 +1,22 @@
+/// @file 20230518_LiveSession3.cpp
+/// @author Adam T Koehler, PhD
+/// @date May 18, 2023
+/// @brief Code and lecture notes from the live session on loops & functions.
+
+// Copyright Notice
+// This document is protected by U.S. copyright law. Reproduction and 
+// distribution of this work, including posting or sharing through any medium, 
+// is explicitly prohibited by law, and also violates UIC's Student 
+// Disciplinary Policy (A2-c. Unauthorized Collaboration; and A2-e3. 
+// Participation in Academically Dishonest Activities: Material Distribution). 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   
+    return 0;
+}
