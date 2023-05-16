c6b2f05f335af3ac1bacdfbeed791a39dffef62a
Adam T Koehler, PhD <akoehler@uic.edu>
2023-05-16 16:51:21 -0500

    initialize notes file for May 17, 2023

---
+++ notes/20230517_LiveSession2.cpp
@@ -0,0 +1,22 @@
+/// @file 20230517_LiveSession2.cpp
+/// @author Adam T Koehler, PhD
+/// @date May 17, 2023
+/// @brief Code and lecture notes from the live session on branches & loops.
+
+// Copyright Notice
+// This document is protected by U.S. copyright law. Reproduction and 
+// distribution of this work, including posting or sharing through any medium, 
+// is explicitly prohibited by law, and also violates UIC's Student 
+// Disciplinary Policy (A2-c. Unauthorized Collaboration; and A2-e3. 
+// Participation in Academically Dishonest Activities: Material Distribution). 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   
+    return 0;
+}
