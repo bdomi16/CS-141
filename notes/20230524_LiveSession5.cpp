5f30328d87e4a49f2e30ce19c93e76597da1d8d9
Coding Rooms <git@codingrooms.io>
2023-05-23 04:33:42 +0000

    week 2 initial notes push

---
+++ notes/20230524_LiveSession5.cpp
@@ -0,0 +1,21 @@
+/// @file 20230524_LiveSession5.cpp
+/// @author Adam T Koehler, PhD
+/// @date May 24, 2023
+/// @brief Code and lecture notes from the live session on vectors & arrays.
+
+// Copyright Notice
+// This document is protected by U.S. copyright law. Reproduction and 
+// distribution of this work, including posting or sharing through any medium, 
+// is explicitly prohibited by law, and also violates UIC's Student 
+// Disciplinary Policy (A2-c. Unauthorized Collaboration; and A2-e3. 
+// Participation in Academically Dishonest Activities: Material Distribution). 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
---
+++ notes/20230525_LiveSession6.cpp
@@ -0,0 +1,21 @@
+/// @file 20230525_LiveSession6.cpp
+/// @author Adam T Koehler, PhD
+/// @date May 25, 2023
+/// @brief Code and lecture notes from the live session on 2D vectors & arrays.
+
+// Copyright Notice
+// This document is protected by U.S. copyright law. Reproduction and 
+// distribution of this work, including posting or sharing through any medium, 
+// is explicitly prohibited by law, and also violates UIC's Student 
+// Disciplinary Policy (A2-c. Unauthorized Collaboration; and A2-e3. 
+// Participation in Academically Dishonest Activities: Material Distribution). 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
